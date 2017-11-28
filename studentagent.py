4a3c6dfdcee26f63a10e03a9b5caa23ab59fc827
Leonardo Costa <leonardocosta@ua.pt>
2017-11-28 23:35:11 +0000

    regression

--- studentagent.py
+++ studentagent.py
@@ -1,20 +1,16 @@
 from agent import *
 import random,math,sys,collections,pickle
 
+
+# This is an example of an agent to play the LongLife game.
+# It is not very intelligent: it simply avoids crashing onto walls or bodies.
+
 class StudentAgent(Agent):
-
-    noFoodNear = False
-    waitForResponse = False
-    otherAgentAlive = True
-    sendingTarget = False
-    appendingTarget = False
-    tToAppend = ()
-
-    init = True
-
     def __init__(self, name, body, world):
         super().__init__(name, body, world)
-        #self.fill_dead_ends()
+        self.number = 0
+        self.sum = 0
+
     def chooseAction(self, vision, msg):
         """Analyse input vision and msg, and choose an action to do and msg to post."""
         # This is the brain of the agent. It should be reimplemented on any subclass.
@@ -25,83 +21,17 @@ class StudentAgent(Agent):
         # (You may want to use pickle.dumps / pickle.loads to convert
         # objects to bytes and back.)
 
-        head = self.body[0]
-        msgToSend = b""
-        targets = []
-        msgReceived = ()
-        self.sendingTarget = False
-
         head = self.body[0] 
-
         validact = self.valid_actions(head, vision)
-
-        food = list(vision.food.items())
-        #Check msg
-        if msg:
-            if(self.waitForResponse):
-                self.waitForResponse = False
-                self.noFoodNear = False
-                msgReceived = pickle.loads(msg)
-                if msg[0] == 2:
-                    self.tToAppend = msg[1]
-                    self.appendingTarget = True
-                else: #Still To DO
-                    self.tToAppend = (self.calculateInverse(head),'S')
-                    self.appendingTarget = True
-            else:
-                self.sendingTarget = True
-        else:
-            if(self.waitForResponse):
-                self.waitForResponse = False
-                self.otherAgentAlive = False
-
-        #Sort food
-        food.sort(key=lambda x: self.distance(head, x[0]))
-
-        #Sending Target
-        if self.sendingTarget:
-            if len(food) > 1:
-                toSend = (2,food[1])
-                msgToSend = pickle.dumps(toSend)
-                #print(self.name + ': Sending Target: '+ str(msgToSend))
-            else:
-                toSend = (3,)
-                msgToSend = pickle.dumps(toSend)
-                #print(self.name + ': Sending No Target Available')
-
-        #Appending Target
-        if self.appendingTarget:
-            #print(self.name + ': appending Target: ' + str(self.tToAppend))
-            goToInverse = False
-            food.append(self.tToAppend)
-            if len(food) > 1 or head == self.tToAppend[0]:
-                self.appendingTarget = False
-
+        
+        food = list(vision.food.keys())
+        food.sort(key=lambda x: self.distance(x, head))
         if food != []:
-            pos = self.search_astar(head, food[0][0], vision)
+            pos = self.search_astar(head, food[0], vision)
             if pos in validact.keys():
-                #print(self.name + ' - ' + str(validact[pos]))
-                return validact[pos], msgToSend
+                return validact[pos], b""
 
-            return Stay,msgToSend
-
-        else: #Send message requiring food coordinates
-            if(self.otherAgentAlive and not self.sendingTarget):
-                toSend = (1,)
-                msgToSend = pickle.dumps(toSend)
-                #print(self.name + ': Request target: ' + str(msgToSend))
-                self.noFoodNear = True
-                self.waitForResponse = True
-                return Stay,msgToSend
-            else: #Go to Inverse TO DO
-                self.tToAppend = (self.calculateInverse(head),'S')
-                self.appendingTarget = True
-                return Stay,msgToSend
-
-    def calculateInverse(self,pos):
-        resX = pos.x - self.world.size.x/2
-        resY = pos.y - self.world.size.y/2
-        return self.world.normalize(Point(resX,resY))
+        return Stay, b""
 
     def distance(self, head, pos):
         dx = abs(head.x - pos.x)
@@ -113,45 +43,45 @@ class StudentAgent(Agent):
         return math.hypot(dx,dy)
 
     def search_astar(self, start, goal, vision):
-        closedset = collections.deque() # coordenadas visitadas
-        openset = collections.deque()
-        openset.append(start) # coordenadas por visitar
+        closedset = list() # coordenadas visitadas
+        openset = [start] # coordenadas por visitar
         cameFrom = dict() # dicionario/mapa, dada uma posicao retorna a posicao anterior
         Gdict = collections.defaultdict(lambda: math.inf) # dicionario/mapa, dada uma posicao retorna o custo acumulado
         Fdict = collections.defaultdict(lambda: math.inf) # dicionario/mapa, dada uma posicao retorna o custo estimado(heuristica)
-        Gdict[start] = 0
+        Gdict[start] = 0 
         Fdict[start] = self.distance(start, goal)
-
-        while len(openset):
-            current = openset.popleft()
-            if goal in cameFrom.keys():
-                return self.first_action(cameFrom, current, start)
-            
+        
+        while openset != []:
+            current = openset[0]
+            if current == goal:
+                return self.first_action(cameFrom, current)
+            openset.remove(current)
             closedset.append(current)
+
             validact = self.valid_actions(current, vision).keys()
-            clos = set(closedset)
-            neighbors = list(pos for pos in validact if pos not in clos)
+            neighbors = list(pos for pos in validact if pos not in closedset) 
             neighbors.sort(key=lambda x: self.distance(x, goal))
-            opens = set(openset)
             for pos in neighbors:
-                if pos not in opens:
+                if pos not in openset:
                     openset.append(pos)
-
+                
                 gscore = Gdict[current] + self.distance(current,pos)
-
+                
                 if gscore < Gdict[pos]:
                     cameFrom[pos] = current
                     Gdict[pos] = gscore
                     Fdict[pos] = Gdict[pos] +self.distance(current,goal)
+                
+        return start
 
-        return None
-
-    def first_action(self, cameFrom, current, start):
-        pos = current
-        while cameFrom[pos] != start:
-            pos = cameFrom[pos]
-        return pos
-
+    def first_action(self, cameFrom, current):
+        first = current
+        second = current
+        while first in cameFrom.keys() and cameFrom[first] != first:
+            second = first
+            first = cameFrom[first]
+        return second
+        
 
     def valid_actions(self, head, vision):
         validact = dict()
@@ -160,18 +90,3 @@ class StudentAgent(Agent):
             if newpos not in self.world.walls and newpos not in vision.bodies:
                 validact[newpos] = act
         return validact
-
-    def fill_dead_ends(self, head, vision):
-        dead_ends = set()
-        translate2 = [Point(1,0), Point(0,1), Point(-1,0), Point(0,-1)]
-        i = 0
-        end_found = True
-        while end_found:
-            for pos in self.world.walls:
-                for i in range(4):
-                    p1 = self.world.translate(translate2[i%4])
-                    p2 = self.world.translate(translate2[(i+1)%4])
-                    p3 = self.world.translate(translate2[(i+2)%4])
-                    if (p1 and p2 and p3) in self.world.walls:
-                        dead_ends.add(pos)
-                        end_found=True
